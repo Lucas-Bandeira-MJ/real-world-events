2323a18a09cc4efddaa875c5db1cc634335fa8f9
lucasbande <lucasbandeira.mj@outlook.com>
2020-04-13 14:23:14 -0300

    first commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'i want to lear vue'
+    }
+  }
+}
+</script>
+
+<style></style>
